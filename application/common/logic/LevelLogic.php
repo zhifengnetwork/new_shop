1931dc0f7837bb751f92ea41b2d5c8932ff8f911
BlizzardWu <2442668602@qq.com>
2019-05-16 20:53:44 +0800

    新：levelLogic

--- application/common/logic/LevelLogic.php
+++ application/common/logic/LevelLogic.php
@@ -19,54 +19,46 @@ class LevelLogic extends Model
         set_time_limit(0);
         $data = file_get_contents("php://input");
 
-        $frist_leader_info = $this->user_info_agent($leaderId);
+        $frist_leader_info = $this->get_up_lists($leaderId); //获取所有上级的id（包括自己）
+//        dump($frist_leader_info);die;
         //最大等级
         $max_level = Db::name('agent_level')->max('level');
-        // $agent_level = M('users')->where('user_id',$agent_id)->value('distribut_level');
-        // $d_info = Db::query("select `user_id` from `tp_users` where 'first_leader' = $leaderId or parents like '%,$leaderId,%'");dump($d_info);
-        // get_down_nums($d_info);
+
         //判断是否有上级,有就升级
         if($frist_leader_info){
             foreach($frist_leader_info as $k=>$v){  
                 //现在等级
-                // $agent_level = M('users')->where('user_id',$v)->value('distribut_level');
-                $agent_level = Db::query("select `distribut_level` from `tp_users` where `user_id` = '$v'");
-                // dump($agent_level);die;
-                $agent_level=$agent_level[0]['distribut_level'];
-                //所有下级列表
-                $d_info = Db::query("select `user_id` from `tp_users` where 'first_leader' = $v or parents like '%,$v,%'");
-                //条件团队人数
-                // $team_nums = Db::name('agent_level')->where('level',$agent_level+1)->value('team_nums');//dump($team_nums);
-                $team_nums = Db::query("select `team_nums` from `tp_agent_level` where `level` = $agent_level+1");
-                $team_nums=$team_nums[0]['team_nums'];
-                $count = 0;
-                // if(!isset($d_info) || empty($d_info)){
-                //     continue;
-                // }
-                foreach($d_info as $k1=>$v1){
-                    //下级等级
-                    $l = Db::name('users')->where('user_id',$v1['user_id'])->value('distribut_level');//dump($l);
-                    // $l = Db::query("select `distribut_level` from `tp_users` where `user_id` = ".$v1['user_id']);
-                    // $l=$l[0]['distribut_level'];
-                    if($l >= $agent_level){
-                        $count += 1;
-                        //如果同级人数达到升级条件规定,跳出到下一步
-                        if($count >= $team_nums){
-                            // continue;
-                            break;
-                        }
-                    }
+                $agent_level = Db::name('users')->where('user_id',$v)->value('distribut_level');
 
+                //所有下级id列表
+//                $d_info = Db::query("select `user_id` from `tp_users` where 'first_leader' = $v or parents like '%,$v,%'");
+                $d_info = Db::query("select `user_id` from `tp_parents_cache` where find_in_set($v,parents)");
+                //升级条件所需团队人数
+                $team_nums = Db::name('agent_level')->where('level',$agent_level+1)->value('team_nums');
+                $count = 0;
+
+                if ($d_info){
+                    foreach($d_info as $k1=>$v1){
+                        //下级等级
+                        $l = Db::name('users')->where('user_id',$v1['user_id'])->value('distribut_level');//dump($l);
+
+                        if($l >= $agent_level){
+                            $count += 1;
+                            //如果同级人数达到升级条件规定,跳出到下一步
+                            if($count >= $team_nums){
+                                // continue;
+                                break;
+                            }
+                        }
+
+                    }
                 }
+                //如果该上级id所有下级的同级人数大于条件所需，执行升级，否则跳出这步循环
                 if($count < $team_nums){
                     continue;
                 }else{
                     $this->upgrade_agent($v,$max_level,$count);
-                    // Db::name('admin_log')->insert(['log_info'=>$v]);
                 }
-                // dump('111d   '.$count);
-                // $num = $this->get_down_nums($v,$d_info);dump($num);            
-                // $this->upgrade_agent($v,$max_level);
             }
         }
     }
@@ -82,7 +74,12 @@ class LevelLogic extends Model
                     ->where('level',$agent_level)->find();
         return $condition;
     }
-    //升级
+    /**
+     * 升级
+     * $agent_id    用户id
+     * $max_level   最大等级
+     * $count       下级中同级别人数
+     */
     public function upgrade_agent($agent_id,$max_level,$count){
         ignore_user_abort(true);
         set_time_limit(0);
@@ -91,8 +88,7 @@ class LevelLogic extends Model
         global $list_test;
         //用户等级
         $agent_level = Db::name('users')->where('user_id',$agent_id)->value('distribut_level');
-        //最大等级
-        // $max_level = Db::name('agent_level')->max('level');
+
         $field = "ind_goods_sum, agent_goods_sum";
         //获取用户业绩
         //所有直推下级总业绩
@@ -105,12 +101,10 @@ class LevelLogic extends Model
         //个人业绩 = 自己业绩 + 所有直推下级总业绩
         $agent_cond['ind_goods_sum'] = $agent_cond['ind_goods_sum'] + $down_nums;
         //团队标准（团队同级人数）
-        // $d_info = Db::query("select `user_id`  from `tp_users` where 'first_leader' = $agent_id or parents like '%,$agent_id,%'");
-        // dump($d_info);
-        // $team_nums = $this->get_down_nums($agent_id,$agent_level,$d_info);
-        $agent_cond['team_nums'] = $count;//$team_nums;
+        $agent_cond['team_nums'] = $count;
         //升级条件
         $condition = $this->condition($agent_level+1);
+
         $bool = true;
         foreach($agent_cond as $k=>$v){
             if($v < $condition[$k]){
@@ -126,128 +120,50 @@ class LevelLogic extends Model
         }
     }
     
-    /**
-     * 获取推荐上级id
-     */
-    public function user_info_agent($user_id)
-    {
-        $lists = Db::name('users')->where('user_id',$user_id)->value('parents');
-        $parents = [];
-        if($lists){
-            $parents = array_filter(explode(',',$lists));
-        }
-
-        array_push($parents,$user_id);
-        $parents = array_reverse($parents);
-        // $recUser  = $this->getAllUp($data);
-        // $list = array_column($recUser,'user_id');
-        return $parents;
-    }
+//    /**
+//     * 获取推荐上级id
+//     */
+//    public function user_info_agent($user_id)
+//    {
+//        $lists = Db::name('users')->where('user_id',$user_id)->value('parents');
+//        $parents = [];
+//        if($lists){
+//            $parents = array_filter(explode(',',$lists));
+//        }
+//
+//        array_push($parents,$user_id);
+//        $parents = array_reverse($parents);
+//        // $recUser  = $this->getAllUp($data);
+//        // $list = array_column($recUser,'user_id');
+//        return $parents;
+//    }
     
-    /**
-     * 获取所有上级
-     */
-    public function getAllUp($invite_id,&$userList=array())
-    {
-        $field  = "user_id,first_leader,is_lock";
-        $UpInfo = M('users')->field($field)->where(['user_id'=>$invite_id])->find();
-        if($UpInfo)  //有上级
-        {
-            $userList[] = $UpInfo;
 
-            $this->getAllUp($UpInfo['first_leader'],$userList);
-        }
-        
-        $list = array_column($userList,'user_id');
-        
-        return $userList;
-    }
-    
-    // /**
-    //  * 获取团队下级id
-    //  */
-    // public function get_team_id($user_id){
-    //     ini_set('max_execution_time', '0');
-    //     global $list_downid;
-    //     $user_level = M('users')->where('user_id',$user_id)->value('distribut_level');
-    //     $all = M('users')->field('user_id,first_leader,distribut_level')->where('first_leader',$user_id)->select();
-    //     $list = array_column($all,'user_id');
-    //     foreach($list as $k =>$v){
-    //         $list_downid[]=$v;
-    //         $this->get_team_id($v);
-    //     }
-       
-    // }
-    // /**
-    //  * 递归获取团队下级同级人数
-    //  */
-    // public function membercount($agent_id)
-    // {
-    //     global $list_downid;
-    //     $test = $this->get_team_id($agent_id);
-    //     $down_level = [];
-    //     $count = 0;
-    //     $agent_level = M('users')->where('user_id',$agent_id)->value('distribut_level');
-    //     //获取下级等级
-    //     if($list_downid){
-    //         foreach($list_downid as $k=>$v){
-    //             $level = M('users')->where('user_id',$v)->value('distribut_level');
-    //             array_push($down_level,$level);
-    //         }
-    //         //下级同等级数
-    //         foreach($down_level as $k1=>$v1){
-    //             if($v1 == $agent_level){
-    //                 $count += 1;
-    //             }
-    //         }
 
-    //     }
-
-    //     return $count;
-        
-    // }
-    /**
-     * 获取团队下级同级人数
-     */
-    public function get_down_nums($agent_id,$d_info)
-    {
-        //获取下级id列表
-        $agent_level = M('users')->where('user_id',$agent_id)->value('distribut_level');
-        // $d_info = Db::query("select `user_id`, `first_leader`,`parents` from `tp_users` where 'first_leader' = $agent_id or parents like '%,$agent_id,%'");
-        // $d_info = Db::query("select `user_id`  from `tp_users` where 'first_leader' = $agent_id or parents like '%,$agent_id,%'");
-        if($d_info){
-            // $id_array =[];
-            $count = 0;
-            foreach($d_info as $k=>$v){
-                // dump($v);    
-                $l = Db::name('users')->where('user_id',$v['user_id'])->value('distribut_level');//dump($l);
-                if($l >= $agent_level){
-                    $count += 1;
-                }       
-                // $l = Db::query("select `distribut_level` from `tp_users` where `user_id` = $v["user_id"]");
-                // $lev_list[$k] = array_column($l,'distribut_level');
-                // array_push($id_array ,$v['user_id']);
-            }
-        }
-        //获取同级人数
-        // if($id_array){
-        //     $lev_list = [];
-        //     foreach($id_array as $k){
-                // if($k){
-                //     $l = Db::query("select `distribut_level` from `tp_users` where `user_id` = $k");
-                //     $lev_list[$k] = array_column($l,'distribut_level');
-                // }
-        //     }
-        //     $count = 0;
-        //     foreach($lev_list as $k1=>$v1){
-        //         if($v1[0] == $agent_level){
-        //             $count += 1;
-        //         }
-        //     }
-        // }
-        // dump($count);
-        return $count;
-    }
+//    /**
+//     * 获取团队下级同级人数
+//     */
+//    public function get_down_nums($agent_id,$d_info)
+//    {
+//        //每个人的等级
+//        $agent_level = M('users')->where('user_id',$agent_id)->value('distribut_level');
+//
+//        if($d_info){
+//            // $id_array =[];
+//            $count = 0;
+//            foreach($d_info as $k=>$v){
+//                // dump($v);
+//                $l = Db::name('users')->where('user_id',$v['user_id'])->value('distribut_level');//dump($l);
+//                if($l >= $agent_level){
+//                    $count += 1;
+//                }
+//                // $l = Db::query("select `distribut_level` from `tp_users` where `user_id` = $v["user_id"]");
+//                // $lev_list[$k] = array_column($l,'distribut_level');
+//                // array_push($id_array ,$v['user_id']);
+//            }
+//        }
+//        return $count;
+//    }
 
     /**
      * 获取所有直推下级业绩
@@ -256,23 +172,40 @@ class LevelLogic extends Model
     {
         //获取直推下级id
         $ids = Db::name('users')->where('first_leader',$agent_id)->field('user_id')->select();
-        // $id_array = [];
         $down_count = 0;
         if($ids){
             foreach($ids as $k=>$v){
-                // dump($v);
                 $count = Db::name('agent_performance')->where('user_id',$v['user_id'])->value('ind_goods_sum');
                 $down_count = $down_count + $count;
-                // array_push($id_array ,$v['user_id']);
             }
         }
-        // dump($id_array);
-        // $down_count = 0;
-        // foreach($id_array as $v){
-        //     $count = Db::name('agent_performance')->where('user_id',$v)->value('ind_goods_sum');
-        //     $down_count = $down_count + $count;
-        // }
         return $down_count;
     }
 
+
+    /**
+     * 新的 获取上级id列表
+     */
+    public function get_up_lists($user_id)
+    {
+        $list = Db::name('parents_cache')->where('user_id',$user_id)->order('sort asc')->field('parents,count')->select();
+        $list1 = [];
+        foreach ($list as $k=>$v){
+            array_push($list1,$v['parents']);
+        }
+        array_push($list1,$user_id);
+        $parents = implode($list1,','); //上级列
+        $lists = array_reverse(array_filter(explode(',',$parents))); //上级列表数组
+
+        return $lists;
+    }
+
+//    /**
+//     * 新的 获取下级id列表
+//     */
+//    public  function get_down_lists($user_id)
+//    {
+//        $d_lists = Db::query("select `user_id` from `tp_parents_cache` where find_in_set($user_id,parents)");
+//        dump($d_lists);
+//    }
 }
