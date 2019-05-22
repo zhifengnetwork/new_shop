34949530767b403533f96f41363eef6e41ad60cb
hsj628 <13690837@163.com>
2019-05-22 10:09:56 +0800

    修改手机验证码验证规则

--- application/common.php
+++ application/common.php
@@ -1819,7 +1819,8 @@ function check_mobile_number($mobile)
     if (!is_numeric($mobile)) {
         return false;
     }
-    $reg = '#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#';
+//    $reg = '#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#';
+    $reg = "/^1[3456789]\d{9}$/";
 
     return preg_match($reg, $mobile) ? true : false;
 }
